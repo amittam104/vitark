3dd8ffc27e67eba6889e9bb65e2acbd6386c4f07
amittambulkar104 <amittambulkar104@gmail.com>
2024-11-15 05:35:57 +0000

    chore: project setup

---
+++ components/Header.tsx
@@ -0,0 +1,13 @@
+
+
+function Header() {
+    return <header className="py-2 max-w-7xl">
+        <div>1</div>
+        <div>
+        <div></div>
+        <div></div>
+            </div>
+    </header>;
+}
+
+export default Header;
