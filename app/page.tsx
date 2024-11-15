b8b184f625b2810eff935da1b2c094439ad2c3f8
amittambulkar104 <amittambulkar104@gmail.com>
2024-11-15 11:24:09 +0000

    chore: update landing page header

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 export default function Home() {
   return (
-    <section className="mx-auto max-w-[90rem] text-center flex justify-center mt-20 pt-20">
-      <h1 className="text-6xl font-light max-w-4xl leading-snug">
-        Library for logical components, Not just UI components
-      </h1>
-    </section>
+    <div></div>
+    // <section className="mx-4 xl:mx-auto max-w-[90rem] text-center flex justify-center mt-20 pt-20">
+    //   <h1 className="text-6xl font-light max-w-4xl leading-snug tracking-tight">
+    //     Library for logical components, Not just UI components
+    //   </h1>
+    // </section>
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,28 +1,80 @@
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { Diamond, GithubLogo, XLogo } from "@phosphor-icons/react/dist/ssr";
+import { PanelRight } from "lucide-react";
 import Link from "next/link";
-import { Input } from "./ui/input";
 import { ThemeToggle } from "./ThemeToggle";
+import { Button } from "./ui/button";
 
 function Header() {
   return (
-    <header className="py-2 w-full mx-auto max-w-[90rem] flex items-center justify-between">
+    <header className="py-2 px-4 flex items-center justify-between gap-8 lg:justify-start h-14 max-w-[90rem] mx-auto">
       <div className="flex items-center gap-2">
         <Diamond size={20} weight="light" />
         <p className="font-light">Vitark</p>
       </div>
-      <div className="flex items-center gap-4">
-        <div className="flex items-center gap-2">
-          <Input />
-        </div>
+
+      {/* Desktop and Laptop navigation */}
+      <div className="hidden lg:flex-1 lg:flex lg:items-center lg:justify-between">
+        <nav>
+          <ul className="flex items-center gap-4 text-sm text-neutral-500">
+            <li>Docs</li>
+            <li>Components</li>
+            <li>Feedback</li>
+          </ul>
+        </nav>
         <div className="flex items-center gap-4">
-          <Link href="https://github.com/amittam104/vitark" target="_blank">
-            <GithubLogo size={20} weight="light" />
-          </Link>
-          <Link href="https://X.com/amittambulkar" target="_blank">
-            <XLogo size={20} weight="light" />
-          </Link>
-          <ThemeToggle />
+          <div className="flex items-center gap-2">
+            <Link href="https://github.com/amittam104/vitark" target="_blank">
+              <Button variant="ghost" size="icon">
+                <GithubLogo size={20} weight="light" />
+              </Button>
+            </Link>
+            <Link href="https://X.com/amittambulkar" target="_blank">
+              <Button variant="ghost" size="icon">
+                <XLogo size={20} weight="light" />
+              </Button>
+            </Link>
+            <ThemeToggle />
+          </div>
         </div>
+      </div>
+
+      {/* Mobile navigation */}
+      <div className="flex items-center lg:hidden">
+        <Sheet>
+          <SheetTrigger>
+            <PanelRight size={20} strokeWidth={1.6} />
+          </SheetTrigger>
+          <SheetContent>
+            <div className="absolute left-4 top-2">
+              <ThemeToggle />
+            </div>
+            <div className="flex flex-col items-end gap-8 mt-10">
+              <nav>
+                <ul className="flex flex-col items-end gap-4 text-sm text-neutral-500">
+                  <li>Docs</li>
+                  <li>Components</li>
+                  <li>Feedback</li>
+                </ul>
+              </nav>
+
+              <div className="flex items-center gap-2">
+                <Link
+                  href="https://github.com/amittam104/vitark"
+                  target="_blank">
+                  <Button variant="ghost" size="icon">
+                    <GithubLogo size={20} weight="light" />
+                  </Button>
+                </Link>
+                <Link href="https://X.com/amittambulkar" target="_blank">
+                  <Button variant="ghost" size="icon">
+                    <XLogo size={20} weight="light" />
+                  </Button>
+                </Link>
+              </div>
+            </div>
+          </SheetContent>
+        </Sheet>
       </div>
     </header>
   );
