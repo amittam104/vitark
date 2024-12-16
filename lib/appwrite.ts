185a2c4023e7c70a8e5b39a53da6b88c6c06427a
amittambulkar104 <amittambulkar104@gmail.com>
2024-12-16 07:52:57 +0000

    fix: feedback page error

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -3,8 +3,8 @@ import { Client, Account, Databases } from "appwrite";
 export const client = new Client();
 
 client
-  .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-  .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID!);
+  .setEndpoint("https://cloud.appwrite.io/v1")
+  .setProject("675e85f80005e72369cf");
 
 export const account = new Account(client);
 export const databases = new Databases(client);
